7ad769f9868f140c018986e9cc679364b88f0866
Erik Ostlund <eostlund74@gmail.com>
2020-07-20 17:21:25 -0500

    Created first GET and POST routes
    Created a mock data file for listings and made an interface for the objects.
    Made Get route to get all listings.
    Made POST route which takes an id, looks for the id and removes the object.
    Installed middleware to parse the json.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { listings } from './listings';
+
 const app = express();
 const port = 9000;
 
-const one = 1;
-const two = 2;
+// apply middleware to parse incoming requests as JSON
+app.use(bodyParser.json());
 
-app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`));
+// GET all listings
+app.get('/listings', (_req, res) => {
+	return res.send(listings);
+});
+
+// POST route to delete a listing
+app.post('/delete-listing', (req, res) => {
+	const id: string = req.body.id;
+
+	for (let i = 0; i < listings.length; i++) {
+		if (listings[i].id === id) {
+			return res.send(listings.splice(i, 1)[0]);
+		}
+	}
+	// if id can't be found
+	return res.send('ERROR: That listing does not exist. Nothing deleted.');
+});
 
 app.listen(port);
 
