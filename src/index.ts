9b2e80e7c86059347d555b082ad15111f1be0e72
Erik Ostlund <eostlund74@gmail.com>
2020-07-25 16:25:35 -0500

    Rewrote GraphQL Schema
    …using GraphQL scema language.
    Deleted old schema and replaced with our new typeDefs and resolvers.
    Added new eslint rule.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { schema } from './graphql';
+import { typeDefs, resolvers } from './graphql';
 
 // create an instance of Express server
 const app = express();
 const port = 9000;
 
 // create an instance of Apollo server
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
