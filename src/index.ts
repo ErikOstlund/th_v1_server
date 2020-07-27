d0e5b97a09a66956d197708483f5c744f852870a
Erik Ostlund <eostlund74@gmail.com>
2020-07-27 17:55:41 -0500

    MongoDB Setup
    Connected new MongoDB to the server with a new ‘mount’ function in the src/index file.
    Added ‘context’ to the ApolloServer instance so all resolvers will have access to the DB.
    Added Typescript to objects coming back from the DB.
    Added environment variables with .env file.
    Updated gitignore.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
-import express from 'express';
+// instead of having this require here, moved it to package.json/scripts
+// require('dotenv').config();
+
+import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import { connectDatabase } from './database';
 import { typeDefs, resolvers } from './graphql';
 
-// create an instance of Express server
-const app = express();
-const port = 9000;
+// The "type" of the Express app instance will be the Application interface
+// that is imported from the express declaration file
+const mount = async (app: Application) => {
+	// run the connectDatabase function
+	const db = await connectDatabase();
 
-// create an instance of Apollo server
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: '/api' });
+	// create an instance of Apollo server
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		context: () => ({ db })
+	});
+	server.applyMiddleware({
+		app,
+		path: '/api'
+	});
 
-app.listen(port);
+	app.listen(process.env.PORT);
 
-console.log(`[app]: http://localhost:${port}`);
+	console.log(`[app]: http://localhost:${process.env.PORT}`);
+
+	// testing the db connection
+	const listings = await db.listings.find({}).toArray();
+	console.log('Listings: ', listings);
+};
+
+mount(express());
 
 // if not using body-parser, can we remove from package.json?
